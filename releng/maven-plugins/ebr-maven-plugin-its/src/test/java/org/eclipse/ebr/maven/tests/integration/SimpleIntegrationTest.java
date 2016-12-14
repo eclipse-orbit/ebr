97e49199217c7f678604403fa4fc93c13b1e999c
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2016-12-14 06:56:13 -0800

    Create separate test plug-in for integration tests
    Signed-off-by: Gunnar Wagenknecht <gunnar@wagenknecht.org>

---
+++ releng/maven-plugins/ebr-maven-plugin-its/src/test/java/org/eclipse/ebr/maven/tests/integration/SimpleIntegrationTest.java
@@ -0,0 +1,48 @@
+package org.eclipse.ebr.maven.tests.integration;
+
+import static io.takari.maven.testing.TestResources.assertFilesPresent;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.takari.maven.testing.TestResources;
+import io.takari.maven.testing.executor.MavenExecutionResult;
+import io.takari.maven.testing.executor.MavenRuntime;
+import io.takari.maven.testing.executor.MavenRuntime.MavenRuntimeBuilder;
+import io.takari.maven.testing.executor.MavenVersions;
+import io.takari.maven.testing.executor.junit.MavenJUnitTestRunner;
+
+@RunWith(MavenJUnitTestRunner.class)
+@MavenVersions({ "3.2.5" })
+public class SimpleIntegrationTest {
+
+	@Rule
+	public final TestResources resources = new TestResources();
+
+	public final MavenRuntime verifier;
+
+	public SimpleIntegrationTest(final MavenRuntimeBuilder builder) throws Exception {
+		verifier = builder.withCliOptions("-X").build();
+	}
+
+	private File getProjectDir(final String project) throws IOException {
+		final File projectDir = resources.getBasedir("it-tests/" + project);
+		assertTrue("Project directory '" + projectDir + "' not found!", projectDir.isDirectory());
+		return projectDir;
+	}
+
+	@Test
+	public void test() throws Exception {
+		final File baseDir = getProjectDir("simple");
+
+		final MavenExecutionResult result = verifier.forProject(baseDir).execute("package");
+
+		result.assertErrorFreeLog();
+		assertFilesPresent(baseDir, "target/MANIFEST.MF");
+	}
+}
