303441fd6c350b1923f267b96404b1f6796a345a
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2018-07-25 13:29:40 +0200

    Logback 1.2.3
    Signed-off-by: Gunnar Wagenknecht <gunnar@wagenknecht.org>

--- recipes/logging/logback/create-logback-recipies.sh
+++ recipes/logging/logback/create-logback-recipies.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-mvn ebr:create-recipe -U -DgroupId=ch.qos.logback -DartifactId=logback-core -DbundleSymbolicName=ch.qos.logback.core.ebr
-mvn ebr:create-recipe -U -DgroupId=ch.qos.logback -DartifactId=logback-classic -DbundleSymbolicName=ch.qos.logback.classic.ebr
-mvn ebr:create-recipe -U -DgroupId=ch.qos.logback -DartifactId=logback-classic -DbundleSymbolicName=ch.qos.logback.slf4j.ebr
+mvn ebr:create-recipe -U -DgroupId=ch.qos.logback -DartifactId=logback-core -DbundleSymbolicName=ch.qos.logback.core.ebr "$@"
+mvn ebr:create-recipe -U -DgroupId=ch.qos.logback -DartifactId=logback-classic -DbundleSymbolicName=ch.qos.logback.classic.ebr "$@"
+mvn ebr:create-recipe -U -DgroupId=ch.qos.logback -DartifactId=logback-classic -DbundleSymbolicName=ch.qos.logback.slf4j.ebr "$@"
 
 echo "\n"
 echo "\n"
